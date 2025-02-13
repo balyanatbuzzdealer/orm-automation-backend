a2dc1d4e619e0dbd61526df72d5c79e557bcece0
David <davidbalyan9@gmail.com>
2025-02-13 12:14:46 +0200

    Changed scrape URL

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
-@app.post("/scrape")
+@app.post("/")
 async def scrape(
     country: str = Form(...),
     search_terms: str = Form(...),
